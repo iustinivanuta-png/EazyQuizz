f75930b0481f979cdbdc3664b38f772df4afc71f
Iustin Ivanuta <iustin.ivanuta@student.usv.ro>
2026-03-17 10:41:20 +0200

    Create Quiz class for knowledge tests

---
+++ Quiz.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace KnowledgeTestSystem.Models
+{
+    public class Quiz
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Domain { get; set; } // Domeniu: Geografie, Biologie, Istorie, etc.
+        public string Description { get; set; }
+        public List<Question> Questions { get; set; } = new List<Question>();
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
