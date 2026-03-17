5d2aff290a8100adf9f3dcf7ab6255a461c90fe2
Iustin Ivanuta <iustin.ivanuta@student.usv.ro>
2026-03-17 10:43:45 +0200

    Create QuizResult.cs

---
+++ QuizResult.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace KnowledgeTestSystem.Models
+{
+    public class QuizResult
+    {
+        public int Id { get; set; }
+        public int QuizId { get; set; }
+        public Quiz Quiz { get; set; }
+        public int CorrectAnswers { get; set; }
+        public int TotalQuestions { get; set; }
+        public double Percentage { get; set; }
+        public DateTime CompletedAt { get; set; } = DateTime.Now;
+    }
+}
